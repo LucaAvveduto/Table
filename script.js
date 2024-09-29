bb64d86ad0d4797fa03d9bae47838eb35290ff30
Luca Avveduto <avvedutoluca@itis-molinari.eu>
2024-09-29 11:48:31 +0200

    applicazione completa

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const createTable = (el) => {
     let data = [];
     return {
         render: () => {
-            let tab = `<table class="class=table table-striped">`;
+            let tab = `<table class="table table-striped">`;
             tab += data.map((row) => 
                 "<tr>" + row.map((col) => 
                 "<td>" + col + "</td>"
@@ -20,8 +20,46 @@ const createTable = (el) => {
     };
 }
 
+const createForm = (el) => {
+    let callback = null;
+    let data;
+    return {
+        setLabel: (info) => {
+            data = info;
+        },
+        onsubmit: (cbk) => {
+            callback = cbk;
+        },
+        render: () => {
+            el.innerHTML =
+            data.map((element,index) => {
+                console.log(element);
+                let type = "text";
+                if(index === 1) type = "number";
+                return `
+                 <label class="form-label">` + element +`</label>
+                 <input type=`+ type + ` class="form-control" id=`+ element + ` placeholder=`+ element +`>
+                `
+            }).join("");
+            el.innerHTML += `<button type="button" class="btn btn-primary" id="sub">SUB</button>`;
+
+            document.querySelector("#sub").onclick = () => {
+                const result = data.map((name) => {
+                    const values = document.querySelector("#" + name).value;
+                    document.querySelector("#" + name).value = ""; 
+                    return values;
+                });
+                callback(result);
+            };
+        }
+    }
+}
+
 const t = createTable(table);
-t.add(["inter","20"]);
-t.add(["inter","20"]);
-t.render();
-console.log(table)
+const f = createForm(form);
+f.setLabel(["squadra","punti"]);
+f.onsubmit((result => {
+    t.add(result);
+    t.render();
+}));
+f.render();
